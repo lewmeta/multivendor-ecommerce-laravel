96784050c88d3e0e35a9e2fe76e3b192876cea11
lewmeta <dev.lewismeta@gmail.com>
2026-02-11 10:51:24 +0300

    Add product routes Add product controller to save product creation

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ProductStoreRequest;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
@@ -46,9 +47,32 @@ class ProductController extends Controller
     /**
      * Create a product
      */
-    public function store(): JsonResponse
+    public function store(ProductStoreRequest $request, string $type): JsonResponse
     {
-        dd('This route is working!');
-    }
+        if (!in_array($type, ['physical', 'digital'])) abort(404);
 
+        $product = new Product();
+        $product->name = $request->name;
+        $product->slug = $request->slug;
+        $product->product_type = $type;
+        $product->short_description = $request->short_description;
+        $product->description = $request->content;
+        $product->sku = $request->sku;
+        $product->price = $request->price;
+        $product->special_price = $request->special_price;
+        $product->special_price_start = $request->from_date;
+        $product->special_price_end = $request->to_date;
+        $product->qty = $request->quantity;
+        $product->manage_stock = $request->has('manage_stock') ? 'yes' : 'no';
+        $product->in_stock = $request->stock_status == 'in_stock' ? 1 : 0;
+        $product->approved_status = 'approved';
+        $product->store_id = $request->store;
+        $product->brand_id = $request->brand;
+        $product->is_featured = $request->has('is_featured') ? 1 : 0;
+        $product->is_hot = $request->has('is_hot') ? 1 : 0;
+        $product->is_new = $request->has('is_new') ? 1 : 0;
+        $product->save();
+
+        
+    }
 }
